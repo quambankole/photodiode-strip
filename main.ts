5bdc1c9fdd5fd0ccce30ad77a055e0a8d794ebee
Quam Bankole <145612653+quambankole@users.noreply.github.com>
2025-03-06 14:45:09 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -5,23 +5,27 @@ basic.showLeds(`
     . . . . .
     . . . . .
     . . . . .
-    `)
+`)
+
 basic.forever(function () {
     touchValue = pins.analogReadPin(AnalogPin.P0)
-    if (touchValue > 600 && touchValue < 630) {
+
+    if (touchValue > 600 && touchValue < 650) {
         basic.showIcon(IconNames.Yes)
-        music.play(music.stringPlayable("C5 A B F A E A D ", 147), music.PlaybackMode.UntilDone)
-    } else if (touchValue > 630 && touchValue < 660) {
+        music.play(music.stringPlayable("C C5 C C5 C C5 C C5 ", 120), music.PlaybackMode.UntilDone)
+    } else if (touchValue > 650 && touchValue < 660) {
         basic.showIcon(IconNames.Happy)
-        music.play(music.stringPlayable("C E C F A F D C ", 120), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("- D D D D D D - ", 120), music.PlaybackMode.UntilDone)
     } else if (touchValue > 660 && touchValue < 680) {
         basic.showIcon(IconNames.Heart)
-        music.play(music.stringPlayable("C5 A G F E D C E ", 92), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("- - - C D C D C ", 120), music.PlaybackMode.UntilDone)
     } else if (touchValue > 680 && touchValue < 750) {
         basic.showIcon(IconNames.SmallHeart)
-        music.play(music.stringPlayable("C5 C5 A A E E G F ", 218), music.PlaybackMode.UntilDone)
+        music.play(music.stringPlayable("D F G D E G D F ", 120), music.PlaybackMode.UntilDone)
     } else {
-        basic.showIcon(IconNames.No)
+        basic.showIcon(IconNames.No) 
     }
-    basic.pause(100)
+
+    basic.pause(100) 
+    basic.clearScreen() 
 })
